04d2dc4cb30d77ddfae2046e0504c1623130b0e8
taoey <hwt8080@163.com>
2022-07-09 18:00:51 +0800

    map 注释测试

--- src/sync/map.go
+++ src/sync/map.go
@@ -9,6 +9,8 @@ import (
 	"unsafe"
 )
 
+// 适用于读多写少场景
+
 // Map is like a Go map[interface{}]interface{} but is safe for concurrent use
 // by multiple goroutines without additional locking or coordination.
 // Loads, stores, and deletes run in amortized constant time.
